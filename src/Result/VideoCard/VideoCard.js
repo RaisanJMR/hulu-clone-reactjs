0746eb964946f897d355f5f71dc9f65f130e7561
RaisanJMR <raisanjmr@protonmail.com>
2021-04-18 16:19:12 +0530

    working on video card

--- src/Result/VideoCard/VideoCard.js
+++ src/Result/VideoCard/VideoCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import './VideoCard.css'
-import Img from './img1.jpg';
-function VideoCard({movie}) {
-  return <div className="videoCard">
-      <img src={Img} alt=""/>
+import './VideoCard.css';
+// import Img from './img1.jpg';
+function VideoCard({ movie }) {
+  return (
+    <div className="videoCard">
+      <img
+        src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/a3liO9nUSCyEhyiAnDOw_Profit-with-JavaScript-new.jpg"
+        alt=""
+      />
+      {/* <img src={Img} alt=""/> */}
       <p>Lorem ipsum dolor sit.</p>
       <h2>movie title</h2>
-      <p>movie overview- Lorem, ipsum dolor</p>
-  </div>;
+      <p>number of likes</p>
+    </div>
+  );
 }
 
 export default VideoCard;
